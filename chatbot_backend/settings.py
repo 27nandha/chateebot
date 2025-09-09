723a93ca3b98f7944745075ba85a585789cf315f
27nandha <nandhakumar272002@gmail.com>
2025-09-09 12:19:16 +0530

    updated

--- chatbot_backend/settings.py
+++ chatbot_backend/settings.py
@@ -123,10 +123,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 import os
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "chatbot/static"),
-]
-
+STATICFILES_DIRS = [BASE_DIR / "chatbot" / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
